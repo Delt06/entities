5c6505cd79e037a4db9e67fc6178b2eb367411d7
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-03-21 13:06:16 +0300

    Add tests for querying extensions

--- Packages/com.deltation.entities/Assets/DELTation/Entities/Runtime/QueryingExtensions.cs
+++ Packages/com.deltation.entities/Assets/DELTation/Entities/Runtime/QueryingExtensions.cs
@@ -11,13 +11,12 @@ namespace DELTation.Entities
 			if (target == null) throw new ArgumentNullException(nameof(target));
 			component = default;
 			return target.TryGetComponent(out IEntity entity) && entity.TryGet(out component);
-		} 
-		
+		}
+
 		public static bool TryGetInEntity<T>([NotNull] this Component target, out T component) where T : class
 		{
 			if (target == null) throw new ArgumentNullException(nameof(target));
-			component = default;
-			return target.TryGetComponent(out IEntity entity) && entity.TryGet(out component);
-		} 
+			return target.gameObject.TryGetInEntity(out component);
+		}
 	}
 }
---
+++ Packages/com.deltation.entities/Assets/DELTation/Entities/Tests/Runtime/QueryingExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace DELTation.Entities.Tests.Runtime
+{
+	internal class QueryingExtensionsTests : CachedEntityTestsBase
+	{
+		[Test]
+		public void TryGetInEntity_HasNoEntity_ReturnsFalse()
+		{
+			var gameObject = new GameObject();
+
+			var hasInEntity = gameObject.TryGetInEntity<Rigidbody>(out _);
+
+			Assert.That(hasInEntity, Is.False);
+		}
+
+		[Test]
+		public void TryGetInEntity_HasEntityButNoComponent_ReturnsFalse()
+		{
+			var hasInEntity = CachedEntity.gameObject.TryGetInEntity<Rigidbody>(out _);
+
+			Assert.That(hasInEntity, Is.False);
+		}
+
+		[Test]
+		public void TryGetInEntity_HasEntityAndComponent_ReturnsTrueAndThatComponent()
+		{
+			var component = CachedEntity.gameObject.AddComponent<Rigidbody>();
+
+			var hasInEntity = CachedEntity.gameObject.TryGetInEntity<Rigidbody>(out var foundComponent);
+
+			Assert.That(hasInEntity);
+			Assert.That(foundComponent, Is.EqualTo(component));
+		}
+
+		[Test]
+		public void TryGetInEntity_GameObjectIsNull_ThrowsArgumentNullException()
+		{
+			GameObject gameObject = null;
+
+			Assert.That(() => gameObject.TryGetInEntity<Rigidbody>(out _), Throws.ArgumentNullException);
+		}
+	}
+}
