20b63db9a8da76337ed038e9e9351c69d11c46fa
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-03-21 11:54:08 +0300

    Add querying extensions

---
+++ Packages/com.deltation.entities/Assets/DELTation/Entities/Runtime/QueryingExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace DELTation.Entities
+{
+	public static class QueryingExtensions
+	{
+		public static bool TryGetInEntity<T>([NotNull] this GameObject target, out T component) where T : class
+		{
+			if (target == null) throw new ArgumentNullException(nameof(target));
+			component = default;
+			return target.TryGetComponent(out IEntity entity) && entity.TryGet(out component);
+		} 
+		
+		public static bool TryGetInEntity<T>([NotNull] this Component target, out T component) where T : class
+		{
+			if (target == null) throw new ArgumentNullException(nameof(target));
+			component = default;
+			return target.TryGetComponent(out IEntity entity) && entity.TryGet(out component);
+		} 
+	}
+}
