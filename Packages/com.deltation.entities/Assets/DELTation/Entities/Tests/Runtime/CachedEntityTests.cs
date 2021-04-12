be9e60e21a85b7a94f41a0647ae225f490984be7
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-04-12 15:03:49 +0300

    Refactor cached entity tests

--- Packages/com.deltation.entities/Assets/DELTation/Entities/Tests/Runtime/CachedEntityTests.cs
+++ Packages/com.deltation.entities/Assets/DELTation/Entities/Tests/Runtime/CachedEntityTests.cs
@@ -13,46 +13,60 @@ namespace DELTation.Entities.Tests.Runtime
 	internal class CachedEntityTests : CachedEntityTestsBase
 	{
 		[Test]
-		public void CachedEntity_GetLocalComponent_ReturnsTheComponent()
+		public void GivenCachedEntity_WhenGettingLocalComponent_ThenTheComponentIsReturned()
 		{
+			// Arrange
 			var rigidbody = CachedEntity.GameObject.AddComponent<Rigidbody>();
-
+			
+			// Act
 			var returnedRigidbody = CachedEntity.Get<Rigidbody>();
 
+			// Assert
 			returnedRigidbody.Should().Be(rigidbody);
 		}
 
 		[Test]
-		public void CachedEntity_GetNonExistingComponent_ThrowsException()
+		public void GivenCachedEntity_WhenGettingNotExistingComponent_ThenThrowsException()
 		{
+			// Arrange
+
+			// Act
+
+			// Assert
 			CachedEntity.Invoking(e => e.Get<Rigidbody>())
 				.Should()
 				.Throw<Exception>();
 		}
 
 		[Test]
-		public void CachedEntity_GetChildComponent_ReturnsTheComponent()
+		public void GivenCachedEntity_WhenGettingChildComponent_ThenTheComponentIsReturned()
 		{
+			// Arrange
 			var child = new GameObject();
 			child.transform.parent = CachedEntity.GameObject.transform;
 			var rigidbody = child.AddComponent<Rigidbody>();
 
+			// Act
 			var returnedRigidbody = CachedEntity.Get<Rigidbody>();
 
+			// Assert
 			returnedRigidbody.Should().Be(rigidbody);
 		}
 
 		[Test]
-		public void CachedEntityWithInactiveSearchEnabled_GetInactiveChildComponent_ReturnsTheComponent()
+		public void GivenInactiveSearchEnabled_WhenInactiveChildComponent_ThenTheComponentIsReturned()
 		{
+			// Arrange
 			var child = new GameObject();
 			child.transform.parent = CachedEntity.GameObject.transform;
 			var rigidbody = child.AddComponent<Rigidbody>();
 			child.SetActive(false);
 			CachedEntity.SearchInInactiveChildren = true;
 
+			// Act
 			var returnedRigidbody = CachedEntity.Get<Rigidbody>();
 
+			// Assert
 			returnedRigidbody.Should().Be(rigidbody);
 		}
 
@@ -71,49 +85,59 @@ namespace DELTation.Entities.Tests.Runtime
 		}
 
 		[UnityTest]
-		public IEnumerator CachedEntityWithDestroyedComponentsRemoval_GetDestroyedComponent_ThrowsException()
+		public IEnumerator GivenDestroyedComponentsRemovalEnabled_WhenGettingDestroyedComponent_ThenThrowsException()
 		{
+			// Arrange
 			CachedEntity.RemoveDestroyedComponents = true;
 			var rigidbody = CachedEntity.GameObject.AddComponent<Rigidbody>();
 			CachedEntity.Get<Rigidbody>();
 
+			// Act
 			Object.Destroy(rigidbody);
 			yield return null;
 
+			// Assert
 			CachedEntity.Invoking(e => e.Get<Rigidbody>())
 				.Should()
 				.Throw<Exception>();
 		}
 
 		[UnityTest]
-		public IEnumerator CachedEntityWithoutDestroyedComponentsRemoval_GetDestroyedComponent_ReturnsNullObject()
+		public IEnumerator GivenDestroyedComponentsRemovalDisabled_WhenGettingDestroyedComponent_ThenNullIsReturned()
 		{
+			// Arrange
 			CachedEntity.RemoveDestroyedComponents = false;
 			var rigidbody = CachedEntity.GameObject.AddComponent<Rigidbody>();
 			CachedEntity.Get<Rigidbody>();
-
+			
+			// Act
 			Object.Destroy(rigidbody);
 			yield return null;
 			var returnedRigidbody = CachedEntity.Get<Rigidbody>();
 
+			// Assert
 			Assert.That(returnedRigidbody == null);
 		}
 
 		[Test]
-		public void CachedEntity_GetManyLocalComponents_ReturnsThemAll()
+		public void GivenCachedEntity_WhenGettingManyLocalComponents_ThenAllOfThemReturned()
 		{
+			// Arrange
 			var colliders = Enumerable.Range(0, 5)
 				.Select(i => CachedEntity.GameObject.AddComponent<BoxCollider>())
 				.ToArray();
 
+			// Act
 			var returnedColliders = CachedEntity.GetMany<BoxCollider>();
 
+			// Assert
 			returnedColliders.Should().BeEquivalentTo(colliders);
 		}
 
 		[Test]
-		public void CachedEntity_GetManyLocalAndNonLocalComponents_ReturnsThemAll()
+		public void GivenCachedEntity_WhenGettingManyLocalAndNonLocalComponents_ThenAllOfThemReturned()
 		{
+			// Arrange
 			var colliders1 = Enumerable.Range(0, 5)
 				.Select(i => CachedEntity.GameObject.AddComponent<BoxCollider>());
 			var child = new GameObject();
@@ -122,32 +146,17 @@ namespace DELTation.Entities.Tests.Runtime
 				.Select(i => child.AddComponent<BoxCollider>());
 			var allColliders = colliders1.Concat(colliders2).ToArray();
 
+			// Act
 			var returnedColliders = CachedEntity.GetMany<BoxCollider>();
 
+			// Assert
 			returnedColliders.Should().BeEquivalentTo(allColliders);
 		}
 
 		[Test]
-		public void CachedEntityWithInactiveSearchEnabled_GetManyInactiveLocalAndNonLocalComponents_ReturnsThemAll()
+		public void GivenInactiveSearchDisabled_WhenGettingManyInactiveLocalAndNonLocalComponents_ThenReturnNone()
 		{
-			CachedEntity.SearchInInactiveChildren = true;
-			var colliders1 = Enumerable.Range(0, 5)
-				.Select(i => CachedEntity.GameObject.AddComponent<BoxCollider>());
-			var child = new GameObject();
-			child.transform.parent = CachedEntity.transform;
-			var colliders2 = Enumerable.Range(0, 5)
-				.Select(i => child.AddComponent<BoxCollider>());
-			var allColliders = colliders1.Concat(colliders2).ToArray();
-			child.SetActive(false);
-
-			var returnedColliders = CachedEntity.GetMany<BoxCollider>();
-
-			returnedColliders.Should().BeEquivalentTo(allColliders);
-		}
-
-		[Test]
-		public void CachedEntityWithInactiveSearchDisabled_GetManyInactiveLocalAndNonLocalComponents_ReturnNone()
-		{
+			// Arrange
 			CachedEntity.SearchInInactiveChildren = false;
 			var child = new GameObject();
 			child.transform.parent = CachedEntity.transform;
@@ -156,61 +165,75 @@ namespace DELTation.Entities.Tests.Runtime
 				.ToArray();
 			child.SetActive(false);
 
+			// Act
 			var returnedColliders = CachedEntity.GetMany<BoxCollider>();
 
+			// Assert
 			returnedColliders.Should().BeEmpty();
 		}
 
 		[UnityTest]
-		public IEnumerator
-			CachedEntityWithoutDestroyedComponentsRemoval_DestroyOneComponentAndGetAll_ReturnsAllIncludingTheDeleted()
+		public IEnumerator GivenDestroyedComponentsRemovalDisabled_WhenDestroyingOneComponentAndGettingAll_ThenAllIncludingTheDeletedAreReturned()
 		{
+			// Arrange
 			CachedEntity.RemoveDestroyedComponents = false;
 			var colliders = Enumerable.Range(0, 5)
 				.Select(i => CachedEntity.GameObject.AddComponent<BoxCollider>())
 				.ToArray();
 			CachedEntity.GetMany<BoxCollider>();
 
+			// Act
 			Object.Destroy(colliders[1]);
 			yield return null;
 			var returnedColliders = CachedEntity.GetMany<BoxCollider>();
 
+			// Assert
 			returnedColliders.Should().BeEquivalentTo(colliders);
 		}
 
 		[UnityTest]
-		public IEnumerator
-			CachedEntityWithDestroyedComponentsRemoval_DestroyOneComponentAndGetAll_ReturnsAllExceptTheDeleted()
+		public IEnumerator GivenDestroyedComponentsRemoval_WhenDestroyingOneComponentAndGettingAll_ThenAllExceptTheDeletedAreReturned()
 		{
+			// Arrange
 			CachedEntity.RemoveDestroyedComponents = true;
 			var colliders = Enumerable.Range(0, 5)
 				.Select(i => CachedEntity.GameObject.AddComponent<BoxCollider>())
 				.ToArray();
 			CachedEntity.GetMany<BoxCollider>();
 
+			// Act
 			Object.Destroy(colliders[0]);
 			yield return null;
 			var returnedColliders = CachedEntity.GetMany<BoxCollider>();
 
+			// Assert
 			returnedColliders.Should().BeEquivalentTo(colliders.Skip(1));
 		}
 
+
 		[Test]
-		public void CachedEntityWithComponent_TryGet_ReturnsTrueAndCorrectComponent()
+		public void GivenEntityWithComponent_WhenTryingToGet_ThenTrueAndCorrectComponentAreReturned()
 		{
+			// Arrange
 			var rigidbody = CachedEntity.GameObject.AddComponent<Rigidbody>();
 
+			// Act
 			var found = CachedEntity.TryGet<Rigidbody>(out var returnedRigidbody);
 
+			// Assert
 			found.Should().BeTrue();
 			returnedRigidbody.Should().Be(rigidbody);
 		}
 
 		[Test]
-		public void CachedEntityWithoutComponent_TryGet_ReturnsFalse()
+		public void GivenEntityWithoutComponents_WhenTryingToGet_ThenFalseReturned()
 		{
+			// Arrange
+
+			// Act
 			var found = CachedEntity.TryGet<Rigidbody>(out _);
 
+			// Assert
 			found.Should().BeFalse();
 		}
 	}
